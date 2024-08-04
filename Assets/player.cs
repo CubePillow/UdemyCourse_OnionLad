1ec25089d801f54cb10b04affcba8b959a7c0881
Erya <eryawang620@gmail.com>
2024-08-04 04:36:48 +0200

    jump and move

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,22 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public float jumpForce;
+    
+
+    private float _xInput;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start was called");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        _xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(_xInput * moveSpeed,rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Jump");
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-        
     }
 }
