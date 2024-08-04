a421c429ebab6b6939b27a522bbcdc565f4f2f1f
Erya <eryawang620@gmail.com>
2024-08-04 06:04:33 +0200

    [SerializeField]

--- Assets/player.cs
+++ Assets/player.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed;
-    public float jumpForce;
+    private Rigidbody2D _rb;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
     
 
     private float _xInput;
@@ -12,17 +12,17 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _xInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(_xInput * moveSpeed,rb.velocity.y);
+        _xInput = Input.GetAxis("Horizontal"); 
+        _rb.velocity = new Vector2(_xInput * moveSpeed,_rb.velocity.y);
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
         }
     }
 }
